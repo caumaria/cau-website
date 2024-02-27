3c5d807d294841c774a78f14e07ac107202b7c68
Cau <cau.ugolini@gmail.com>
2024-02-27 18:09:59 -0300

    style[navbar]: layout

--- cau-website/src/app/layout.tsx
+++ cau-website/src/app/layout.tsx
@@ -1,9 +1,10 @@
-'use client'
+"use client";
 import "./globals.css";
 import i18next from "i18next";
 import global_en from "../app/data/translation/en/global.json";
 import global_ptbr from "../app/data/translation/ptbr/global.json";
 import { I18nextProvider } from "react-i18next";
+import Navbar from "./component/Navbar/Navbar";
 
 i18next.init({
   interpolation: { escapeValue: true },
@@ -25,8 +26,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head title="Developer Cau website & blog"/>
       <I18nextProvider i18n={i18next}>
+        <head title="Developer Cau website & blog" />
+        <Navbar />
         <body>{children}</body>
       </I18nextProvider>
     </html>
--- cau-website/src/app/page.tsx
+++ cau-website/src/app/page.tsx
@@ -1,9 +1,7 @@
-import Navbar from "../app/component/Navbar/Navbar";
 
 export default function Home() {
   return (
     <main>
-      <Navbar />
       <p>Hello World!</p>
     </main>
   );
