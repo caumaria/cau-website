8d981d3de9d98a42fc97b13e18209a386947dfaa
Cau <cau.ugolini@gmail.com>
2024-03-08 15:56:58 -0300

    feat[Hero]: add spline 3d sphere

--- cau-website/src/app/component/Hero/Hero.jsx
+++ cau-website/src/app/component/Hero/Hero.jsx
@@ -1,15 +1,23 @@
 "use client";
 import React from "react";
+import { useTranslation } from "react-i18next";
 import Spline from "@splinetool/react-spline";
 import styled from "styled-components";
 
 const HeroContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 800px;
 `;
 
 export default function Hero() {
+  const [t, i18n] = useTranslation("global");
+
   return (
     <HeroContainer>
-      <Spline scene="https://prod.spline.design/oJ7o7XdnnuDg4Il5/scene.splinecode" />
+      <Spline scene="https://prod.spline.design/NnJl03L3RAQdQoIo/scene.splinecode" />
     </HeroContainer>
   );
 }
--- cau-website/src/app/page.tsx
+++ cau-website/src/app/page.tsx
@@ -3,13 +3,11 @@ import Hero from "./component/Hero/Hero";
 import styled from "styled-components";
 
 const HomeContainer = styled.div`
-  margin-top: 20rem;
 `;
 
 export default function Home() {
   return (
     <HomeContainer>
-      <p>Hello World!</p>
       <Hero />
     </HomeContainer>
   );
